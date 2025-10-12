009a9b743126f03cc352dfe0d0bd6b85910e395a
alansouls <maiaalan1111@gmail.com>
2025-10-12 20:23:59 +0100

    add event details to inbox message

--- src/framebox/FrameBox.Core/Inbox/Defaults/DefaultInboxMessageFactory.cs
+++ src/framebox/FrameBox.Core/Inbox/Defaults/DefaultInboxMessageFactory.cs
@@ -6,24 +6,30 @@ namespace FrameBox.Core.Inbox.Defaults;
 
 public class DefaultInboxMessageFactory : IInboxMessageFactory
 {
+    private readonly IDomainEventSerializer _domainEventSerializer;
     private readonly IEventHandlerProvider _eventHandlerProvider;
     private readonly TimeProvider _timeProvider;
     private readonly IEventRegistry _eventRegistry;
 
-    public DefaultInboxMessageFactory(IEventHandlerProvider eventHandlerProvider, TimeProvider timeProvider, IEventRegistry eventRegistry)
+    public DefaultInboxMessageFactory(IEventHandlerProvider eventHandlerProvider, TimeProvider timeProvider, IEventRegistry eventRegistry, IDomainEventSerializer domainEventSerializer)
     {
         _eventHandlerProvider = eventHandlerProvider;
         _timeProvider = timeProvider;
         _eventRegistry = eventRegistry;
+        _domainEventSerializer = domainEventSerializer;
     }
 
-    public IEnumerable<InboxMessage> CreateMessages(IEvent @event)
+    public async ValueTask<IEnumerable<InboxMessage>> CreateMessages(IEvent @event, CancellationToken cancellationToken)
     {
+        var eventPayload = await _domainEventSerializer.SerializeAsync(@event, cancellationToken);
+        
         var handlers = _eventHandlerProvider.GetEventHandlerTypes(@event.GetType());
 
         return handlers.Select(handlerType => new InboxMessage(
             Guid.NewGuid(),
             @event.Id,
+            _eventRegistry.GetEventName(@event.GetType()),
+            eventPayload,
             _eventRegistry.GetHandlerName(handlerType),
             _timeProvider.GetUtcNow()));
     }
