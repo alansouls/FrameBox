b46bcf90cee223cd01074c434c70e4c522af410c
Alan Maia <maiaalan@hotmail.com>
2025-10-16 18:41:49 +0100

    feat: add in process message broker (#15)
    Co-authored-by: Alan Maia <maiaalan1111@gmail.com>

---
+++ src/samples/basic/FrameBox.MessageBroker.InProcess/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using FrameBox.Core.Common.Interfaces;
+using FrameBox.Core.Inbox.Models;
+using FrameBox.Core.Outbox.Models;
+using FrameBox.MessageBroker.AzureServiceBus.Options;
+using FrameBox.MessageBroker.AzureServiceBus.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Threading.Channels;
+
+namespace FrameBox.MessageBroker.RabbitMQ.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    private static Channel<OutboxMessage>? _outboxChannel = null;
+    private static Channel<InboxMessage>? _inboxChannel = null;
+    private static Channel<OutboxMessage> OutboxChannel => _outboxChannel ??= Channel.CreateUnbounded<OutboxMessage>();
+    private static Channel<InboxMessage> InboxChannel => _inboxChannel ??= Channel.CreateUnbounded<InboxMessage>();
+    /// <summary>
+    /// Requires a ServiceBusClient to be registered in the service collection.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddInProcessMessageBroker(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.TryAddSingleton(OutboxChannel.Writer);
+        services.TryAddSingleton(InboxChannel.Writer);
+        services.AddScoped<IMessageBroker, InProcessBroker>();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Requires a ServiceBusClient to be registered in the service collection.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddInProcessListener(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.TryAddSingleton(OutboxChannel.Reader);
+        services.TryAddSingleton(InboxChannel.Reader);
+        services.AddHostedService<InProcessListener>();
+
+        return services;
+    }
+}
---
+++ src/samples/basic/FrameBox.MessageBroker.InProcess/Services/InProcessBroker.cs
@@ -0,0 +1,63 @@
+﻿using FrameBox.Core.Common.Exceptions;
+using FrameBox.Core.Common.Interfaces;
+using FrameBox.Core.Inbox.Models;
+using FrameBox.Core.Outbox.Models;
+using FrameBox.MessageBroker.AzureServiceBus.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System.Threading.Channels;
+
+namespace FrameBox.MessageBroker.AzureServiceBus.Services;
+
+public class InProcessBroker : IMessageBroker
+{
+    private readonly InProcessOptions _options;
+    private readonly ChannelWriter<OutboxMessage> _outboxWriter;
+    private readonly ChannelWriter<InboxMessage> _inboxWriter;
+
+    public InProcessBroker(ChannelWriter<OutboxMessage> outboxChannel, ChannelWriter<InboxMessage> inboxChannel, IOptions<InProcessOptions> options)
+    {
+        _options = options.Value;
+        _outboxWriter = outboxChannel;
+        _inboxWriter = inboxChannel;
+    }
+
+    public async Task SendMessagesAsync<T>(IEnumerable<T> messages, CancellationToken cancellationToken) where T : class, IMessage
+    {
+        var messageList = messages.ToList();
+
+        if (messageList.Count == 0)
+        {
+            return;
+        }
+
+        var failedMessages = new List<T>();
+        foreach (var message in messageList)
+        {
+            try
+            {
+                if (message is OutboxMessage outboxMessage)
+                {
+                    await _outboxWriter.WriteAsync(outboxMessage, cancellationToken);
+                }
+                else if (message is InboxMessage inboxMessage)
+                {
+                    await _inboxWriter.WriteAsync(inboxMessage, cancellationToken);
+                }
+                else
+                {
+                    throw new InvalidOperationException("Message must be of type OutboxMessage or InboxMessage");
+                }
+            }
+            catch (Exception)
+            {
+                failedMessages.Add(message);
+            }
+        }
+
+        if (failedMessages.Count > 0)
+        {
+            throw new FailedToSendMessagesException<T>(failedMessages);
+        }
+    }
+}
